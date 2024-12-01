437faaa4132b21bd1f0f9a63c234a62482a82126
SlightlyBuggy <myers.hunter@gmail.com>
2024-12-01 13:58:38 -0600

    moved to object factory pattern for tool button, which vastly simplified ToolButton.tsx.  need to consider reworking this to be a Component factory, where components are returned directly.

--- app/ui/ImageUploadAndEditor/ImageEditor/ToolButton.tsx
+++ app/ui/ImageUploadAndEditor/ImageEditor/ToolButton.tsx
@@ -1,78 +1,21 @@
 import { DrawingTool } from "@/app/lib/util/enums";
-import clsx from "clsx";
+import { toolButtonStyleFactory } from "./classes/ToolButtonObj";
 
-// TODO: inverse video when tool is selected
-// TODO: can each tool be its own class where event handlers, button style, etc all come together?
-// right now button stuff is in this file.  event handlers are in ImageEditor/lib
-// this may need to wait until we have more tools and can see what all abstractions we need
+// TODO: inverse video when tool is selected?
 
 export default function ToolButton(
     {tool, activeTool, handleToolClick}: 
     {tool: DrawingTool, 
      activeTool: DrawingTool | null, 
      handleToolClick: (tool: DrawingTool) => void}) {
+        
+    const drawingToolObj = toolButtonStyleFactory(tool, activeTool);
 
-        const handleClick = (tool: DrawingTool) => {
-            handleToolClick(tool);
-        }
-    // TODO: with this log statement we can see there are lots of extraneous re-renders.  need to reduce.
-    // console.log(`Active tool is ${activeTool}`)
-
-    switch(tool) {
-        case DrawingTool.Circle:
-            return (
-            <div className="p-3 flex items-center" onClick={() => handleClick(DrawingTool.Circle)}>
-                <div className={
-                    clsx(
-                        'h-5 w-5 rounded-full hover:cursor-pointer',
-                        {'bg-green-300': activeTool == DrawingTool.Circle},
-                        {'hover:bg-yellow-100 bg-green-500': activeTool != DrawingTool.Circle},
-                    )}
-                >
-                </div>
+    return (
+        <div className="p-3 flex items-center" onClick={() => handleToolClick(tool)}>
+            <div className={drawingToolObj.outerStyle}>
+                <div className={drawingToolObj.innerStyle}></div>
             </div>
-            )
-        case DrawingTool.Square:
-            return (
-            <div className="p-3 flex items-center" onClick={() => handleClick(DrawingTool.Square)}>
-                <div className={
-                    clsx(
-                        'h-6 w-6 hover:cursor-pointer',
-                        {'bg-green-300': activeTool == DrawingTool.Square},
-                        {'hover:bg-yellow-100 bg-yellow-500': activeTool != DrawingTool.Square},
-                    )}
-                >
-                </div>
-            </div>
-            )
-        case DrawingTool.Line:
-            return (
-            <div className="p-3 flex items-center" onClick={() => handleClick(DrawingTool.Line)}>
-                <div className={
-                    clsx(
-                        'h-6 w-6 flex items-center group  hover:cursor-pointer',
-                        {'bg-green-300': activeTool == DrawingTool.Line},
-                        {'hover:bg-yellow-100': activeTool != DrawingTool.Line},
-                    )}
-                >
-                    <div className="h-0.5 w-6 bg-black rotate-45 group-hover:bg-gray-500">
-                    </div>
-                </div>
-            </div>
-            )
-        case DrawingTool.RadiusedCircle:
-            return (
-                <div className="p-3 flex items-center" onClick={() => handleClick(DrawingTool.RadiusedCircle)}>
-                    <div className={
-                        clsx(
-                        'h-6 w-6 flex items-center justify-center hover:cursor-pointer',
-                        {'bg-green-300': activeTool == DrawingTool.RadiusedCircle},
-                        {'hover:bg-yellow-100 hover:border-gray-500': activeTool != DrawingTool.RadiusedCircle}
-                     )}
-                >
-                    <div className="h-5 w-5 rounded-full border-solid border-2 border-black"></div>
-                </div>
-            </div>
-            )
-    }
+        </div>
+        )
 }
---
+++ app/ui/ImageUploadAndEditor/ImageEditor/classes/ToolButtonObj.ts
@@ -0,0 +1,78 @@
+import { DrawingTool } from "@/app/lib/util/enums";
+
+abstract class ToolButtonStyles {
+    private activeTool: DrawingTool | null;
+    abstract thisToolType: DrawingTool;
+
+    private outerSharedStyle = "hover:cursor-pointer"
+    private outerActiveStyle = "bg-red-500"
+
+    protected abstract outerFixedStyle: string;
+    protected abstract outerInactiveStyle: string;
+    
+    public abstract innerStyle: string;
+
+    get outerStyle()  {
+
+        const outerStyle = this.outerSharedStyle + " " + this.outerFixedStyle
+
+        if(this.thisToolType === this.activeTool)
+        {
+            return outerStyle + " " + this.outerActiveStyle
+        }
+        return outerStyle + " " + this.outerInactiveStyle
+    }
+
+    constructor(theActiveTool: DrawingTool | null) {
+        this.activeTool = theActiveTool
+    }
+}
+
+class CircleToolButtonStyles extends ToolButtonStyles {
+    thisToolType = DrawingTool.Circle
+
+    outerFixedStyle = "h-5 w-5 rounded-full"
+    protected outerInactiveStyle = "hover:bg-yellow-100 bg-green-500"
+    innerStyle = ""
+}
+
+class SquareToolButtonStyles extends ToolButtonStyles {
+    thisToolType = DrawingTool.Square
+
+    outerFixedStyle = "h-6 w-6"
+    protected outerInactiveStyle = "hover:bg-yellow-100 bg-yellow-300"
+    innerStyle = ""
+}
+
+class LineToolButtonStyles extends ToolButtonStyles {
+    thisToolType = DrawingTool.Line
+
+    outerFixedStyle = "h-6 w-6 flex items-center group"
+    protected outerInactiveStyle = "hover:bg-yellow-100"
+    innerStyle = "h-0.5 w-6 bg-black rotate-45 group-hover:bg-gray-500"
+}
+
+class RadiusedCircleButtonStyles extends ToolButtonStyles {
+    thisToolType = DrawingTool.RadiusedCircle
+
+    outerFixedStyle = "h-6 w-6 flex items-center justify-center"
+    protected outerInactiveStyle = "hover:bg-yellow-100"
+    innerStyle = "h-5 w-5 rounded-full border-solid border-2 border-black"
+}
+
+export const toolButtonStyleFactory = (thisToolType: DrawingTool, activeToolType: DrawingTool | null): ToolButtonStyles =>
+{
+    switch(thisToolType)
+    {
+        case(DrawingTool.Circle):
+            return new CircleToolButtonStyles(activeToolType);
+        case(DrawingTool.Square):
+            return new SquareToolButtonStyles(activeToolType);
+        case DrawingTool.Line:
+            return new LineToolButtonStyles(activeToolType);
+        case DrawingTool.RadiusedCircle:
+            return new RadiusedCircleButtonStyles(activeToolType);
+        default:
+            throw new Error(`toolButtonObjFactory does not handle tool type ${thisToolType}`)
+    }
+}
