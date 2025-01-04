047278fc645b1598c12f893cbb6dda16025d9fe6
SlightlyBuggy <myers.hunter@gmail.com>
2025-01-04 15:10:48 -0600

    made color selector button stand out a bit more (in case the user picks a color close to background)

--- app/ui/ImageUploadAndEditor/ImageEditor/DrawingToolPalette/ColorSelection/SelectedColorButton.tsx
+++ app/ui/ImageUploadAndEditor/ImageEditor/DrawingToolPalette/ColorSelection/SelectedColorButton.tsx
@@ -7,12 +7,13 @@ export default function SelectedColorButton({
   openColorSelector: () => void;
   selectedColor: string;
 }) {
+  // TODO: set bg color dynamically.  if selected color is below a certain value, set it to something dark so the picker is still visible.  otherwise leave it as-is
   return (
     <div
-      className="flex items-center pl-3 pr-3"
+      className="flex items-center pl-3 pr-3 "
       onClick={() => openColorSelector()}
     >
-      <div className="flex items-center justify-center h-6 w-6 hover:cursor-pointer hover:bg-yellow-300">
+      <div className="flex items-center justify-center h-6 w-6 hover:cursor-pointer hover:bg-yellow-300 bg-gray-300">
         <div
           className="h-4 w-4"
           style={{ backgroundColor: selectedColor }}
